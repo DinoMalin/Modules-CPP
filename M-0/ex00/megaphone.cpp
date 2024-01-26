37a47c7f478fa8c2d3136f629f2724c01ac3789c
DinoMalin <jcario@student.42lehavre.fr>
2024-01-26 12:55:37 +0100

    ~ | Updated ex00

--- M-0/ex00/megaphone.cpp
+++ M-0/ex00/megaphone.cpp
@@ -7,10 +7,7 @@ int main(int ac, char **av) {
 		for (int i = 1; i < ac; i++) {
 			for (int j = 0; av[i][j]; j++)
 				std::cout << (char) toupper(av[i][j]);
-			if (i < ac - 1)
-				std::cout << ' ';
 		}
 	}
 	std::cout << std::endl;
 }
-
