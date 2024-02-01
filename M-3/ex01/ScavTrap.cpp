9eead54084db86e0b922e96d8e2a2d2f2d2762f1
DinoMalin <jcario@student.42lehavre.fr>
2024-02-01 15:21:45 +0100

    + | Added ex01 | M3

---
+++ M-3/ex01/ScavTrap.cpp
@@ -0,0 +1,51 @@
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap() : ClapTrap() {
+	this->hitPoints = 100;
+	this->energyPoints = 50;
+	this->attackDamage = 20;
+}
+ScavTrap::ScavTrap(const std::string &name) : ClapTrap(name) {
+	this->hitPoints = 100;
+	this->energyPoints = 50;
+	this->attackDamage = 20;
+	std::cout << "ScavTrap " << this->name << " has been called." << std::endl;
+}
+ScavTrap::~ScavTrap() {
+	std::cout << "ScavTrap " << this->name << " has been destroyed." << std::endl;
+}
+
+void ScavTrap::attack(const std::string &target) {
+	if (this->hitPoints > 0 && this->energyPoints > 0) {
+		std::cout << "ScavTrap " << this->name
+				<< " attacks " << target << ", causing "
+				<< this->attackDamage << " points of damage!"
+				<< std::endl;
+		this->energyPoints--;
+	} else if (this->hitPoints <= 0) {
+		std::cout << "ScavTrap " << this->name
+				  << " is dead, he can't attack anymore."
+				  << std::endl;
+	} else {
+		std::cout << "ScavTrap " << this->name
+			<< " has no more energy, he can't attack anymore."
+			<< std::endl;
+	}
+}
+
+void ScavTrap::guardGate() {
+	if (this->hitPoints > 0 && this->energyPoints > 0) {
+		std::cout << "ScavTrap " << this->name
+				<< " activated Gate keeper mode."
+				<< std::endl;
+		this->energyPoints--;
+	} else if (this->hitPoints <= 0) {
+		std::cout << "ScavTrap " << this->name
+				  << " is dead, he can't activate Gate keeper anymore."
+				  << std::endl;
+	} else {
+		std::cout << "ScavTrap " << this->name
+			<< " has no more energy, he can't activate Gate keeper anymore."
+			<< std::endl;
+	}
+}
---
+++ M-3/ex01/main.cpp
@@ -0,0 +1,15 @@
+#include "ScavTrap.hpp"
+
+int main() {
+	ScavTrap clapOne("Dinozaur");
+
+	clapOne.attack("Steve");
+	clapOne.takeDamage(90);
+	clapOne.beRepaired(20);
+	clapOne.takeDamage(50);
+	clapOne.attack("Steve");
+
+	ScavTrap clapTwo("Robo");
+	for (int i = 0; i < 3; i++)
+		clapTwo.attack("Steve");
+}
