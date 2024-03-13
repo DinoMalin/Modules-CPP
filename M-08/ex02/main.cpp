a65313a1d50e502fd7b34c2eae269d02af14029c
DinoMalin <jcario@student.42lehavre.fr>
2024-03-13 17:32:50 +0100

    ~ | Put all tests in same main

--- M-08/ex02/main.cpp
+++ M-08/ex02/main.cpp
@@ -31,36 +31,34 @@ int main()
 		++it;
 	}
 	std::stack<int> s(mstack);
+
+	std::cout << std::endl;
+
+	std::list<int> list;
+
+	list.push_back(5);
+	list.push_back(17);
+
+	std::cout << list.back() << std::endl;
+
+	list.pop_back();
+
+	std::cout << list.size() << std::endl;
+
+	list.push_back(3);
+	list.push_back(5);
+	list.push_back(737);
+	list.push_back(0);
+
+	std::list<int>::iterator itL = list.begin();
+	std::list<int>::iterator itLe = list.end();
+
+	++itL;
+	--itL;
+	while (itL != itLe)
+	{
+		std::cout << *itL << std::endl;
+		++itL;
+	}
 	return 0;
 }
-
-// int main()
-// {
-// 	std::list<int> mstack;
-	
-// 	mstack.push_back(5);
-// 	mstack.push_back(17);
-
-// 	std::cout << mstack.back() << std::endl;
-
-// 	mstack.pop_back();
-
-// 	std::cout << mstack.size() << std::endl;
-
-// 	mstack.push_back(3);
-// 	mstack.push_back(5);
-// 	mstack.push_back(737);
-// 	mstack.push_back(0);
-
-// 	std::list<int>::iterator it = mstack.begin();
-// 	std::list<int>::iterator ite = mstack.end();
-
-// 	++it;
-// 	--it;
-// 	while (it != ite)
-// 	{
-// 		std::cout << *it << std::endl;
-// 		++it;
-// 	}
-// 	return 0;
-// }
