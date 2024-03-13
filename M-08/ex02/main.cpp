e946e340143ed198b1b1f6aa454e70a47dc67270
DinoMalin <jcario@student.42lehavre.fr>
2024-03-13 17:30:08 +0100

    + | Added test

--- M-08/ex02/main.cpp
+++ M-08/ex02/main.cpp
@@ -33,3 +33,34 @@ int main()
 	std::stack<int> s(mstack);
 	return 0;
 }
+
+// int main()
+// {
+// 	std::list<int> mstack;
+	
+// 	mstack.push_back(5);
+// 	mstack.push_back(17);
+
+// 	std::cout << mstack.back() << std::endl;
+
+// 	mstack.pop_back();
+
+// 	std::cout << mstack.size() << std::endl;
+
+// 	mstack.push_back(3);
+// 	mstack.push_back(5);
+// 	mstack.push_back(737);
+// 	mstack.push_back(0);
+
+// 	std::list<int>::iterator it = mstack.begin();
+// 	std::list<int>::iterator ite = mstack.end();
+
+// 	++it;
+// 	--it;
+// 	while (it != ite)
+// 	{
+// 		std::cout << *it << std::endl;
+// 		++it;
+// 	}
+// 	return 0;
+// }
