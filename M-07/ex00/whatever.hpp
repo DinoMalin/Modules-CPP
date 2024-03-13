4adf02f731e04b24819064872559277d7cdc8646
DinoMalin <jcario@student.42lehavre.fr>
2024-03-13 15:03:05 +0100

    ~ | Used references

--- M-07/ex00/whatever.hpp
+++ M-07/ex00/whatever.hpp
@@ -10,11 +10,11 @@ void swap(T &a, T &b) {
 }
 
 template <typename T>
-T min(T &a, T &b) {
+T &min(T &a, T &b) {
 	return a < b ? a : b;
 }
 
 template <typename T>
-T max(T &a, T &b) {
+T &max(T &a, T &b) {
 	return a > b ? a : b;
 }
